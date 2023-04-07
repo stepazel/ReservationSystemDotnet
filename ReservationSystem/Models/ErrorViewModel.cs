a0aaa4900cde560e5cb9f5ad5fac2625c76eae5e
zelenka <zelenka@cngroup.dk>
2023-04-07 22:10:36 +0200

    init

---
+++ ReservationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ReservationSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
