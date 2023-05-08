089273fb676d1a950825b9342a17d61c0a0aff21
zelenka <zelenka@cngroup.dk>
2023-05-08 18:35:01 +0200

    .

--- ReservationSystem/Controllers/HomeController.cs
+++ ReservationSystem/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using ReservationSystem.Data;
+using ReservationSystem.Data.Entities;
 using ReservationSystem.Models;
+using ReservationSystem.Models.Reservation;
 
 namespace ReservationSystem.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    
+    private readonly ApplicationDbContext _dbContext;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
     public IActionResult Index()
@@ -28,4 +34,29 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
     }
+    
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create([FromForm] CreateReservationModel model)
+    {
+        // TODO create a error handling(logging, flashmessaage) middleware
+        // TODO logger
+
+        if (IsDateInPast(model.Date))
+            throw new Exception($"Date {model.Date} is in past.");
+
+        var reservationDo = new ReservationDo
+        {
+            Email = model.Email,
+            Date = model.Date,
+            Fee = model.Fee,
+            Location = model.Location
+        };
+        _dbContext.Reservations.Add(reservationDo);
+        _dbContext.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    private static bool IsDateInPast(DateTime dateTime) => dateTime.Date < DateTime.Now;
 }
